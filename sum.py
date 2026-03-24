c0521d4420bc0e600063942e5e3512ce748fcabb
Naim Khatib <144950830+naimkhatib@users.noreply.github.com>
2026-03-23 21:33:28 -0600

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,15 @@
+
+count=0
+sum=0
+max=0
+m=input("Enter x: ")
+while m !='9999':
+    #for i in range(4):
+        count+=1
+        sum+=int(m)
+        if max<=int(m):
+         max=int(m)
+        m=input("Enter x: ")
+print("count=",count,"sum=",sum, "max=",max)
+print("avg=",sum/count)
+      
